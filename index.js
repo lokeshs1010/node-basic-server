2d9a7005bfc121f9458ddf51e677b61a03d57252
lokeshs1010 <lokesh1010saini@gmail.com>
2018-07-07 22:06:25 +0530

    My first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const hbs = require('hbs');
+
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log(err);
+        }
+    })
+
+    next();
+})
+
+// app.use((req, res, next) => {
+//     res.render('maintain.hbs');
+// })
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt' , (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+})
+
+app.get('/', (req,res) => {
+    res.render('home.hbs', {
+        pageTitle : 'Home Page',
+        welcomeMessage : 'Welcome to home page'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is on port 3000')
+});
